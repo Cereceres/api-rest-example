e9a165246ab519cf92765af78ba933ee61f5bec9
cereceres <jesus.edelcereceres@gmail.com>
2017-08-17 16:36:37 -0500

    the request and offer stores are added

--- stores/offer/schema.js
+++ stores/offer/schema.js
@@ -5,6 +5,7 @@ module.exports = {
         index: true,
         ref: 'user'
     },
+<<<<<<< HEAD
     createdAt: {
         type: Date
     },
@@ -34,6 +35,38 @@ module.exports = {
         type: Object
     },
     workDurationUom: {
+=======
+    createdAt:{
+        type: Date
+    },
+    fulfillmentMethod:{
+        type:Object
+    },
+    location:{
+        type:Object
+    },
+    media:{
+        type:Object
+    },
+    price:{
+        type:Object
+    },
+    requestId:{
+        type:String,
+        required: true
+    },
+    serviceId:{
+        type:String,
+        required: true
+    },
+    updatedAt:{
+        type:Date
+    },
+    workDuration:{
+        type:Object
+    },
+    workDurationUom:{
+>>>>>>> the request and offer stores are added
         type: String,
         enum: [ 'hour', 'day', 'week' ]
     },
