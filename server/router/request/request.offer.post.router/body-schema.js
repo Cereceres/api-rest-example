7346552ef8f359711996efad6a90e754c58c1641
cereceres <jesus.edelcereceres@gmail.com>
2017-08-19 15:51:32 -0500

    the test are not ready yet

--- server/router/request/request.offer.post.router/body-schema.js
+++ server/router/request/request.offer.post.router/body-schema.js
@@ -1,13 +1,14 @@
 const joi = require('joi');
 
 
-const workDurationUom = ['hour', 'day', 'week'];
+const workDurationUom = [ 'hour', 'day', 'week' ];
 
 module.exports = joi.object().keys({
-    fulfillmentMethod: joi.object(),
-    location: joi.object(),
-    media: joi.object(),
-    price: joi.object(),
-    workDuration: joi.object(),
-    workDurationUom: joi.string().valid(workDurationUom)
+    fulfillmentMethod:joi.object(),
+    location:joi.object(),
+    media:joi.object(),
+    price:joi.object(),
+    requestId:joi.string().required(),
+    workDuration:joi.object(),
+    workDurationUom:joi.string().valid(workDurationUom)
 });
