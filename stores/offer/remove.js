492d09e410c8f0ee14d929543dea41c87a4c846f
cereceres <jesus.edelcereceres@gmail.com>
2017-08-19 15:51:32 -0500

    the test are not ready yet

--- stores/offer/remove.js
+++
@@ -1,7 +0,0 @@
-const catchingErrorFromPromise = require('../../lib/catching-error-from-promise');
-
-
-module.exports = (client) => (query) => {
-    const update = { isActive: false };
-    return catchingErrorFromPromise(client.update(query, update).exec());
-};
