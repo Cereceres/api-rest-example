8e33c1c439f377c5f026f01f6c34b9137d4887b7
cereceres <jesus.edelcereceres@gmail.com>
2017-08-19 15:51:32 -0500

    the test are not ready yet

--- stores/offer/unset.js
+++
@@ -1,7 +0,0 @@
-const catchingErrorFromPromise = require('../../lib/catching-error-from-promise');
-
-module.exports = (client) => (query = {}, data = {}, options = {}) => catchingErrorFromPromise(
-    client
-        .update(query, { $unset: data }, options)
-        .exec()
-);
