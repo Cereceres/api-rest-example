d48ca69dc783b9dd2a4b7a1ae44585c94b52feaa
cereceres <jesus.edelcereceres@gmail.com>
2017-08-24 16:43:51 -0500

    syntaxis is fixed

--- server/lib/middelwares/auth.js
+++ server/lib/middelwares/auth.js
@@ -2,11 +2,8 @@ const { findOne } = require('../../../stores/user');
 const unless = require('./unless');
 
 const middelware = async (ctx, next) => {
-    console.log('In auth = ', ctx.state.user, ctx.session);
     const userUsingJwt = await findOne(ctx.queryToFindUserById);
-
     const userUsingSession = await findOne({ _id: ctx.session.id });
-
     let isAuth = userUsingJwt || userUsingSession;
 
     if (userUsingJwt && userUsingSession) isAuth = isAuth &&
