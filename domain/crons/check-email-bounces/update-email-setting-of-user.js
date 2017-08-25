5e22506602d1c1af3ac1352942f81f412c8a69a9
cereceres <jesus.edelcereceres@gmail.com>
2017-08-24 21:20:31 -0500

    the cron is init

---
+++ domain/crons/check-email-bounces/update-email-setting-of-user.js
@@ -0,0 +1,24 @@
+const {update: updateUser} = require('../../../stores/user');
+
+
+const bounced = true;
+const sendEmail = false;
+const valid = false;
+
+module.exports = async function(bounces) {
+    const bouncesToDelete = [];
+    for (let i = 0; i < bounces.length; i++) {
+        const {email, reason} = bounces[i];
+        const isEmail = {bounced, reason, valid};
+        const {error} = await updateUser({email}, {isEmail, sendEmail})
+            .catch((error) => {
+                console.log(`${error } on `, bounces[i]);
+                return {error};
+            });
+
+        if (!error) bouncesToDelete.push(email);
+    }
+
+    return bouncesToDelete;
+}
+;
