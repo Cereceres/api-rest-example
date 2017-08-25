c86e10e2dd8207c1698e8fa8f5e37b3d3843bdc6
cereceres <jesus.edelcereceres@gmail.com>
2017-08-24 21:20:31 -0500

    the cron is init

---
+++ domain/crons/check-email-bounces/delete-bounces.js
@@ -0,0 +1,13 @@
+module.exports = async (sg, bouncesToDelete) => {
+    const requestToDeleteBounces = sg.emptyRequest({
+        method: 'DELETE',
+        path: '/v3/suppression/bounces',
+        body: {
+            emails: bouncesToDelete
+        }
+    });
+
+    const {body} = await sg.API(requestToDeleteBounces);
+
+    return body;
+};
