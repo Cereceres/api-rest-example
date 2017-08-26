89dd04ef849c5888a64fe02872d3998538c76e35
cereceres <jesus.edelcereceres@gmail.com>
2017-08-25 21:23:03 -0500

    the test to bounce is fixed

--- domain/schedule-tasks/check-email-invalid.js
+++ domain/schedule-tasks/check-email-invalid.js
@@ -1,6 +1,6 @@
 const { get: getInvalid, delete: deleteInvalid } = require('../../services/email/invalid');
 
-const { updateEmailInvalid:updateEmailSettingsOfUser} = require('../../stores/user');
+const { updateEmailInvalid: updateEmailSettingsOfUser } = require('../../stores/user');
 
 module.exports = async () => {
     const spams = await getInvalid();
