0de3610aaa0661ff5b567cee355aab290cbc40d2
cereceres <jesus.edelcereceres@gmail.com>
2017-09-01 18:04:59 -0500

    the 404 message from update is addd

--- server/router/checkr/checkr.post.router/body-schema.js
+++ server/router/checkr/checkr.post.router/body-schema.js
@@ -1,6 +1,7 @@
 const joi = require('joi');
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 module.exports = joi.object().keys({
     firstName: joi.string(),
     middleName: joi.string(),
@@ -24,4 +25,17 @@ module.exports = joi.object().keys({
     workDuration: joi.object(),
     workDurationUom: joi.string().valid(workDurationUom)
 >>>>>>> the check andpoint is updated
+=======
+module.exports = joi.object().keys({
+    firstName: joi.string(),
+    middleName: joi.string(),
+    lastName: joi.string(),
+    email: joi.string(),
+    phone: joi.string(),
+    zipcode: joi.string(),
+    dob: joi.date(),
+    ssn:joi.string(),
+    driverLicenseNumber: joi.string(),
+    driverLicenseState: joi.string()
+>>>>>>> the 404 message from update is addd
 });
