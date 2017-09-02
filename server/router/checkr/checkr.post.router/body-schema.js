c4d9d6b00b7062bd41bf81ab52ba022f42b79ec1
cereceres <jesus.edelcereceres@gmail.com>
2017-09-01 19:01:10 -0500

    the test are fixed

--- server/router/checkr/checkr.post.router/body-schema.js
+++ server/router/checkr/checkr.post.router/body-schema.js
@@ -1,7 +1,5 @@
 const joi = require('joi');
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 module.exports = joi.object().keys({
     firstName: joi.string(),
     middleName: joi.string(),
@@ -13,29 +11,4 @@ module.exports = joi.object().keys({
     ssn:joi.string(),
     driverLicenseNumber: joi.string(),
     driverLicenseState: joi.string()
-=======
-
-const workDurationUom = ['hour', 'day', 'week'];
-
-module.exports = joi.object().keys({
-    fulfillmentMethod: joi.object(),
-    location: joi.object(),
-    media: joi.object(),
-    price: joi.object(),
-    workDuration: joi.object(),
-    workDurationUom: joi.string().valid(workDurationUom)
->>>>>>> the check andpoint is updated
-=======
-module.exports = joi.object().keys({
-    firstName: joi.string(),
-    middleName: joi.string(),
-    lastName: joi.string(),
-    email: joi.string(),
-    phone: joi.string(),
-    zipcode: joi.string(),
-    dob: joi.date(),
-    ssn:joi.string(),
-    driverLicenseNumber: joi.string(),
-    driverLicenseState: joi.string()
->>>>>>> the 404 message from update is addd
 });
