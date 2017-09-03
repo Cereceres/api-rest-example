95e66f5a5634713c56a4f42c8f6527b1a95a573a
cereceres <jesus.edelcereceres@gmail.com>
2017-09-03 11:02:08 -0500

    tets to pagination are not ready

--- server/router/request/request.get.router/index.js
+++ server/router/request/request.get.router/index.js
@@ -1,10 +1,11 @@
 const { getrequest } = require('../../../controllers/request');
 const body = require('./body-schema');
 const params = require('./params-schema');
+const query = require('./query-schema');
 const validate = require('koa2-validation');
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/request/:idRequest', validate({ body, params }), getrequest);
+router.get('/(request|requests)/:idRequest', validate({ body, params, query}), getrequest);
 
 module.exports = router;
--- test/big/request/get-request.test.js
+++ test/big/request/get-request.test.js
@@ -33,6 +33,35 @@ describe('User requests', () => {
             assert.deepEqual(res.media, body.media);
             assert.deepEqual(res.tags, body.tags);
         });
+
+        it('/requests GET sohuld create a request given', async() => {
+            const body = {
+                userId: __user._id,
+                category:{
+                    type: 'Object'
+                },
+                location:{
+                    type: 'Object'
+                },
+                media:{
+                    type: 'Object'
+                },
+                minPrice:1,
+                maxPrice:1,
+                scheduleDate:1,
+            };
+            const requestCreated = await create(body);
+            console.log('requestCreated ', requestCreated);
+            const { body: { request: res } } = await agent.get(`/requests/${requestCreated._id}`)
+                .send(body)
+                .set(authorizationHeader)
+                .set(Cookie)
+                .expect(200);
+            console.log('res = ', res);
+            assert.deepEqual(res.message, body.message);
+            assert.deepEqual(res.media, body.media);
+            assert.deepEqual(res.tags, body.tags);
+        });
     });
 
     describe('FAIL', () => {
