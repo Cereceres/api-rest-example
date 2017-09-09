56695db4bf2c6550322e256100559b5badb59e86
cereceres <jesus.edelcereceres@gmail.com>
2017-09-08 19:16:48 -0500

    the order endpoint is added

--- stores/order/schema.js
+++ stores/order/schema.js
@@ -6,13 +6,65 @@ module.exports = {
         ref: 'user'
     },
     createdAt: {
-        type: Date,         default: new Date()
+        type: Date,
+        default: new Date()
+    },
+    buyerId: {
+        type: String,
+        required: true,
+        index: true,
+        ref: 'user'
+    },
+    buyerOrderDispute: {
+        type: Object
+    },
+    buyerServiceLocation: [ location ],
+    cancellationDate: Date,
+    category: {
+        type: {},
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
     },
     isActive: {
         type: Boolean,
         default: true
     },
-    updatedAt: {
-        type: Date,         default: new Date()
+    fulfillmentMethod: fulfillmentMethod,
+    geofence: {
+        type: Array
+    },
+    orderAcceptanceDate: Date,
+    orderItems: [ orderedItem ],
+    orderMilestoneStatuses: orderMilestoneStatuses,
+    paymentDate: Date,
+    paymentMethod: {
+        type: Object,
+        required: true
+    },
+    servicesPrices: [ price ],
+    transactionFee: Number,
+    transactionDate: Date,
+    updatedAt: Date,
+    sellerAcceptedScheduleTime: Boolean,
+    sellerAcceptedBuyerServiceLocation: Boolean,
+    sellerDeliveredMedia: [ media ],
+    sellerId: {
+        type: String,
+        required: true,
+        index: true,
+        ref: 'user'
+    },
+    sellerServiceLocation: [ location ],
+    serviceScheduleDate: Date,
+    serviceStartDate: Date,
+    serviceCompleteDate: Date,
+    shippingInfo: {
+        type: Object
+    },
+    taxes: {
+        type: Object
     }
 };
