043c427b0e7d1cd99b4e37264f6222a6daccdb2f
cereceres <jesus.edelcereceres@gmail.com>
2017-09-18 17:23:56 -0500

    test to checkr are added

--- stores/user/update.js
+++
@@ -1,9 +0,0 @@
-const catchingErrorFromPromise = require('../../lib/catching-error-from-promise');
-
-module.exports = (client) => (query = {}, data = {}, options = {new:true}) => {
-    try {
-        return catchingErrorFromPromise(client.findOneAndUpdate(query, { $set: data }, options).exec());
-    } catch (error) {
-        return {error};
-    }
-};
